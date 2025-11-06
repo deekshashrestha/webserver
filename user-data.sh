6b35aa8a79ae59488a868e86ed1b0a41b6c0771c
deekshashrestha <deekshashrestha123@gmail.com>
2025-11-06 16:32:15 +0545

    Initial commit

--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update -y
 apt install -y nginx
-echo "<h1>Hello from Terraform + " > /var/www/html/index.html
+echo "<h1>Hello Diksha " > /var/www/html/index.html
 systemctl start nginx
 systemctl enable nginx
